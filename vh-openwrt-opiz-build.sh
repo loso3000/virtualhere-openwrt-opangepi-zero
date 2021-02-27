3b0769dfd283d2eecdf602b0a0f2a91545733b7c
filimonic <alexey@filimonic.net>
2021-02-27 03:08:15 +0300

    initial

--- vh-openwrt-opiz-build.sh
+++ vh-openwrt-opiz-build.sh
@@ -4,23 +4,32 @@ OPENWRT_BUILD_DIR=`mktemp --directory --suffix -openwrt-buildenv`
 
 #apt-get update
 #apt-get install build-essential libncurses5-dev libncursesw5-dev zlib1g-dev gawk git gettext libssl-dev xsltproc wget unzip python
-mkdir --parent ${OPENWRT_BUILD_DIR}/ib
+cd ${OPENWRT_BUILD_DIR}
+
 echo "Downloading OpenWrt ImageBuilder ..."
+mkdir --parent ${OPENWRT_BUILD_DIR}/ib
 wget --quiet --show-progress --progress=bar:force --output-document ${OPENWRT_BUILD_DIR}/ib.tar.xz https://downloads.openwrt.org/releases/${OPENWRT_TARGET_VER}/targets/sunxi/cortexa7/openwrt-imagebuilder-${OPENWRT_TARGET_VER}-sunxi-cortexa7.Linux-x86_64.tar.xz
 echo "Extracting OpenWrt ImageBuilder ..."
 tar --extract --strip-components=1 --directory ${OPENWRT_BUILD_DIR}/ib --file ${OPENWRT_BUILD_DIR}/ib.tar.xz
 echo "Removing OpenWrt ImageBuilder archive"
 rm ${OPENWRT_BUILD_DIR}/ib.tar.xz
+
+
+echo "Downloading patches"
+mkdir --parent ${OPENWRT_BUILD_DIR}/p
+git clone --quiet https://github.com/filimonic/virtualhere-openwrt-opangepi-zero.git ${OPENWRT_BUILD_DIR}/p
+
+echo "Applying patches"
+cp --recursive --force --target-directory ${OPENWRT_BUILD_DIR}/p/files  ${OPENWRT_BUILD_DIR}/ib
+chmod +x ${OPENWRT_BUILD_DIR}/ib/files/usr/bin/vhusbd*
+chmod +x ${OPENWRT_BUILD_DIR}/ib/files/etc/init.d/vhusbd*
+
+
 cd ${OPENWRT_BUILD_DIR}/ib
-
-
-
 echo "Downloading VirtualHere server (unoptimized) ..."
 wget --quiet --show-progress --progress=bar:force --output-document ./files/usr/bin/vhusbd           https://virtualhere.com/sites/default/files/usbserver/vhusbdarm
 echo "Downloading VirtualHere server (optimized) ..."
 wget --quiet --show-progress --progress=bar:force --output-document ./files/usr/bin/vhusbd-optimized https://virtualhere.com/sites/default/files/usbserver/vhusbdarmpi2
-chmod +x ./files/usr/bin/vhusbd*
-chmod +x ./files/etc/init.d/vhusbd*
 
 mkdir --parent ${OPENWRT_TARGET_DIR}
 
